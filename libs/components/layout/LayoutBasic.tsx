65660fd6f21288168e128bb543434331f9558a82
Azizbek Siddikov <azbeksid@gmail.com>
2025-11-22 13:40:14 +0900

    refactor: delete Chat

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -1,25 +1,23 @@
-import { NextPage } from 'next';
-import React from 'react';
+import { NextPage } from "next";
+import React from "react";
 
-import useDeviceDetect from '@/libs/hooks/useDeviceDetect';
-import Header from '@/libs/components/layout/Header';
-import Footer from '@/libs/components/layout/Footer';
-import MobileHeader from '@/libs/components/layout/MobileHeader';
-import MobileFooter from '@/libs/components/layout/MobileFooter';
-import Chat from '@/libs/components/Chat';
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import Header from "@/libs/components/layout/Header";
+import Footer from "@/libs/components/layout/Footer";
+import MobileHeader from "@/libs/components/layout/MobileHeader";
+import MobileFooter from "@/libs/components/layout/MobileFooter";
 
 const withBasicLayout = (Page: NextPage) => {
 	const WrappedComponent = (props: Record<string, unknown>) => {
 		const device = useDeviceDetect();
 
-		if (device === 'mobile')
+		if (device === "mobile")
 			return (
 				<div className="min-h-screen flex flex-col">
 					<MobileHeader />
 					<main className="flex-grow flex items-center justify-center">
 						<Page {...props} />
 					</main>
-					{/* <Chat /> */}
 					<MobileFooter />
 				</div>
 			);
@@ -30,13 +28,12 @@ const withBasicLayout = (Page: NextPage) => {
 				<main className="flex-1">
 					<Page {...props} />
 				</main>
-				{/* <Chat /> */}
 				<Footer />
 			</div>
 		);
 	};
 
-	WrappedComponent.displayName = `withBasicLayout(${Page.displayName || Page.name || 'Component'})`;
+	WrappedComponent.displayName = `withBasicLayout(${Page.displayName || Page.name || "Component"})`;
 	return WrappedComponent;
 };
 
--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,25 +1,23 @@
-import { NextPage } from 'next';
-import React from 'react';
+import { NextPage } from "next";
+import React from "react";
 
-import Header from '@/libs/components/layout/Header';
-import MobileHeader from '@/libs/components/layout/MobileHeader';
-import Footer from '@/libs/components/layout/Footer';
-import MobileFooter from '@/libs/components/layout/MobileFooter';
-import useDeviceDetect from '@/libs/hooks/useDeviceDetect';
-import Chat from '@/libs/components/Chat';
+import Header from "@/libs/components/layout/Header";
+import MobileHeader from "@/libs/components/layout/MobileHeader";
+import Footer from "@/libs/components/layout/Footer";
+import MobileFooter from "@/libs/components/layout/MobileFooter";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 
 const withHomeLayout = (Page: NextPage) => {
 	const WrappedComponent = (props: Record<string, unknown>) => {
 		const device = useDeviceDetect();
 
-		if (device === 'mobile')
+		if (device === "mobile")
 			return (
 				<div className="min-h-screen flex flex-col">
 					<MobileHeader />
 					<main className="flex-grow flex items-center justify-center">
 						<Page {...props} />
 					</main>
-					{/* <Chat /> */}
 					<MobileFooter />
 				</div>
 			);
@@ -30,13 +28,12 @@ const withHomeLayout = (Page: NextPage) => {
 				<main className="flex-1">
 					<Page {...props} />
 				</main>
-				{/* <Chat /> */}
 				<Footer />
 			</div>
 		);
 	};
 
-	WrappedComponent.displayName = `withHomeLayout(${Page.displayName || Page.name || 'Component'})`;
+	WrappedComponent.displayName = `withHomeLayout(${Page.displayName || Page.name || "Component"})`;
 	return WrappedComponent;
 };
 
